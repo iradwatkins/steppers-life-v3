d94b4cf78d4ef643999e4f1fb2e2a558edefa6c5
Ira Watkins <iradwatkins@gmail.com>
2025-06-03 14:09:34 -0400

    fix: Update service worker revision for PWA - Modified the revision identifier for the service worker in sw.js to ensure proper caching and updates for the PWA functionality.

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-f1e82ed9'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.come4dlrn1o"
+    "revision": "0.nmcl9i0loj"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
